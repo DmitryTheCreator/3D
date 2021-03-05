5bfe0e2666381d74c540cd3782c9982c60575e60
DmitryTheCreator <dmitriyzamorev@gmail.com>
2021-03-05 18:13:51 +0500

    Initial commit

---
+++ 3D/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿
+namespace _3D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Tools = new System.Windows.Forms.Panel();
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.btnTextPolygon = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnPolygons = new System.Windows.Forms.Button();
+            this.btnPolygon = new System.Windows.Forms.Button();
+            this.btnEllipse = new System.Windows.Forms.Button();
+            this.btnBezier = new System.Windows.Forms.Button();
+            this.btnBeziers = new System.Windows.Forms.Button();
+            this.btnEllipseSepment = new System.Windows.Forms.Button();
+            this.btnDiagram = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.Tools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.Canvas);
+            this.panel1.Controls.Add(this.Tools);
+            this.panel1.Location = new System.Drawing.Point(14, 14);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1200, 603);
+            this.panel1.TabIndex = 0;
+            // 
+            // Tools
+            // 
+            this.Tools.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Tools.BackColor = System.Drawing.Color.Coral;
+            this.Tools.Controls.Add(this.button1);
+            this.Tools.Controls.Add(this.button2);
+            this.Tools.Controls.Add(this.button3);
+            this.Tools.Controls.Add(this.button4);
+            this.Tools.Controls.Add(this.button5);
+            this.Tools.Controls.Add(this.button6);
+            this.Tools.Controls.Add(this.button7);
+            this.Tools.Controls.Add(this.button8);
+            this.Tools.Controls.Add(this.button9);
+            this.Tools.Controls.Add(this.btnEllipse);
+            this.Tools.Controls.Add(this.btnTextPolygon);
+            this.Tools.Controls.Add(this.btnLine);
+            this.Tools.Controls.Add(this.btnBezier);
+            this.Tools.Controls.Add(this.btnPolygons);
+            this.Tools.Controls.Add(this.btnPolygon);
+            this.Tools.Controls.Add(this.btnBeziers);
+            this.Tools.Controls.Add(this.btnEllipseSepment);
+            this.Tools.Controls.Add(this.btnDiagram);
+            this.Tools.Location = new System.Drawing.Point(835, 0);
+            this.Tools.Name = "Tools";
+            this.Tools.Size = new System.Drawing.Size(365, 603);
+            this.Tools.TabIndex = 0;
+            // 
+            // Canvas
+            // 
+            this.Canvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(832, 603);
+            this.Canvas.TabIndex = 1;
+            // 
+            // btnTextPolygon
+            // 
+            this.btnTextPolygon.Location = new System.Drawing.Point(3, 472);
+            this.btnTextPolygon.Name = "btnTextPolygon";
+            this.btnTextPolygon.Size = new System.Drawing.Size(178, 61);
+            this.btnTextPolygon.TabIndex = 17;
+            this.btnTextPolygon.Text = "Текст и Полигон";
+            this.btnTextPolygon.UseVisualStyleBackColor = true;
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(3, 3);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(178, 61);
+            this.btnLine.TabIndex = 10;
+            this.btnLine.Text = "Линия";
+            this.btnLine.UseVisualStyleBackColor = true;
+            // 
+            // btnPolygons
+            // 
+            this.btnPolygons.Location = new System.Drawing.Point(3, 405);
+            this.btnPolygons.Name = "btnPolygons";
+            this.btnPolygons.Size = new System.Drawing.Size(178, 61);
+            this.btnPolygons.TabIndex = 18;
+            this.btnPolygons.Text = "Полигоны";
+            this.btnPolygons.UseVisualStyleBackColor = true;
+            // 
+            // btnPolygon
+            // 
+            this.btnPolygon.Location = new System.Drawing.Point(3, 338);
+            this.btnPolygon.Name = "btnPolygon";
+            this.btnPolygon.Size = new System.Drawing.Size(178, 61);
+            this.btnPolygon.TabIndex = 19;
+            this.btnPolygon.Text = "Полигон";
+            this.btnPolygon.UseVisualStyleBackColor = true;
+            // 
+            // btnEllipse
+            // 
+            this.btnEllipse.Location = new System.Drawing.Point(3, 539);
+            this.btnEllipse.Name = "btnEllipse";
+            this.btnEllipse.Size = new System.Drawing.Size(178, 61);
+            this.btnEllipse.TabIndex = 11;
+            this.btnEllipse.Text = "Эллипс";
+            this.btnEllipse.UseVisualStyleBackColor = true;
+            // 
+            // btnBezier
+            // 
+            this.btnBezier.Location = new System.Drawing.Point(3, 137);
+            this.btnBezier.Name = "btnBezier";
+            this.btnBezier.Size = new System.Drawing.Size(178, 61);
+            this.btnBezier.TabIndex = 12;
+            this.btnBezier.Text = "Кривая Бельзе";
+            this.btnBezier.UseVisualStyleBackColor = true;
+            // 
+            // btnBeziers
+            // 
+            this.btnBeziers.Location = new System.Drawing.Point(3, 70);
+            this.btnBeziers.Name = "btnBeziers";
+            this.btnBeziers.Size = new System.Drawing.Size(178, 61);
+            this.btnBeziers.TabIndex = 15;
+            this.btnBeziers.Text = "Кривые Бельзе";
+            this.btnBeziers.UseVisualStyleBackColor = true;
+            // 
+            // btnEllipseSepment
+            // 
+            this.btnEllipseSepment.Location = new System.Drawing.Point(3, 204);
+            this.btnEllipseSepment.Name = "btnEllipseSepment";
+            this.btnEllipseSepment.Size = new System.Drawing.Size(178, 61);
+            this.btnEllipseSepment.TabIndex = 14;
+            this.btnEllipseSepment.Text = "Сегмент Эллипса";
+            this.btnEllipseSepment.UseVisualStyleBackColor = true;
+            // 
+            // btnDiagram
+            // 
+            this.btnDiagram.Location = new System.Drawing.Point(3, 271);
+            this.btnDiagram.Name = "btnDiagram";
+            this.btnDiagram.Size = new System.Drawing.Size(178, 61);
+            this.btnDiagram.TabIndex = 13;
+            this.btnDiagram.Text = "Диаграмма";
+            this.btnDiagram.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 539);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 61);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Эллипс";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 472);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 61);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "Текст и Полигон";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(184, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 61);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Линия";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(184, 137);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(178, 61);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Кривая Бельзе";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(184, 405);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(178, 61);
+            this.button5.TabIndex = 27;
+            this.button5.Text = "Полигоны";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(184, 338);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(178, 61);
+            this.button6.TabIndex = 28;
+            this.button6.Text = "Полигон";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(184, 70);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(178, 61);
+            this.button7.TabIndex = 25;
+            this.button7.Text = "Кривые Бельзе";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(184, 204);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(178, 61);
+            this.button8.TabIndex = 24;
+            this.button8.Text = "Сегмент Эллипса";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(184, 271);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(178, 61);
+            this.button9.TabIndex = 23;
+            this.button9.Text = "Диаграмма";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(1228, 630);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.Tools.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.Panel Tools;
+        private System.Windows.Forms.Button btnTextPolygon;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnPolygons;
+        private System.Windows.Forms.Button btnPolygon;
+        private System.Windows.Forms.Button btnEllipse;
+        private System.Windows.Forms.Button btnBezier;
+        private System.Windows.Forms.Button btnBeziers;
+        private System.Windows.Forms.Button btnEllipseSepment;
+        private System.Windows.Forms.Button btnDiagram;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
